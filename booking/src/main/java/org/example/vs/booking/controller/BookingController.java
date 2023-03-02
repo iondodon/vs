b065eb0c468eb41cecbd0265e8becc2195d403cc
Ion Dodon <ion.dodon@amdaris.com>
2023-03-02 12:23:37 +0200

    refactored the boostrap base package

--- booking/src/main/java/org/example/vs/booking/controller/BookingController.java
+++ booking/src/main/java/org/example/vs/booking/controller/BookingController.java
@@ -1,13 +1,12 @@
 package org.example.vs.booking.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.example.vs.booking.controller.request.CreateBookingRequest;
 import org.example.vs.booking.presenter.BookingPresenter;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/bookings")
--- bootstrap/src/main/java/org/example/vs/VsApplication.java
+++ bootstrap/src/main/java/org/example/vs/VsApplication.java
@@ -1,17 +1,13 @@
-package org.example.vs.bootstrap;
+package org.example.vs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.TimeZone;
 
-@EntityScan(basePackages = "org.example.vs")
-@EnableJpaRepositories(basePackages = "org.example.vs")
-@SpringBootApplication(scanBasePackages = "org.example.vs")
+@SpringBootApplication
 public class VsApplication {
     public static void main(String[] args) {
         SpringApplication.run(VsApplication.class, args);
