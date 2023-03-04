b4f2555151d5d161efc205b7728410b1480456aa
Ion Dodon <ion.dodon@amdaris.com>
2023-03-04 11:45:12 +0200

    Added spock framework

---
+++ bootstrap/src/test/groovy/org/example/vs/TestingServiceSpec.groovy
@@ -0,0 +1,13 @@
+package org.example.vs
+
+
+import spock.lang.Specification
+
+class TestingServiceSpec extends Specification {
+
+    def "Name"() {
+        expect: "1 to be equal to 1"
+        1 == 1
+    }
+
+}
